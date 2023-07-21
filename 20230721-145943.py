0bd6cc75bbe9f3a8965795d22f09b914067fb7bf
Harsh ranjan <67137451+harshra215@users.noreply.github.com>
2023-07-21 16:00:34 +0530

    Add files via upload

---
+++ 20230721-145943.py
@@ -0,0 +1,174 @@
+#firstcomment
+print("Hello , darkness")
+numbers=[1,2,3,4,5,67,89,100]
+for x in numbers :
+        print(x)
+
+
+     
+x=5
+y="John"
+print(x)
+x=str(5)
+y=int(5)
+z=float(5)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+ad1f=5
+print(ad1f)
+x="""ORANGES
+     ARE
+     ORANGE IN
+     COLOUR
+     """
+print(x)
+x="Hello ,world!"
+print(x[0])
+print(len(x))
+x="Hello,from the world"
+print("the" in x)
+print("Hi" in x)
+x="Hello, world"
+print(x[2:5])
+print(x[-5:-2])
+a=10
+b=2
+print(a*b)
+print(b-a)
+print(a%b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(x+'yellow')
+c='john'
+d=x    
+print(c+d)
+i=1
+print(i)
+numbers=[1,2,3,4,5,6,67,89,100]
+for x in numbers :
+       print(x)
+print(a-b)
+print(a+b)
+#ordered,changeable and allow duplicates list
+list=["car" ,30,40,3,0,True]
+print(list)
+print(len(list))
+print(type(list))
+print(list[5])
+list[2]="scooters"
+ 
+print(list)
+
+list.insert(3,"jeep")
+
+print(list)
+
+list.append("oranges")
+print(list)
+#ordered,unchangeable and allow duplicates
+tuple=("car",'bike',30,40,3,0,True)
+print(tuple)
+print(type(tuple))
+print(len(tuple))
+
+print(tuple[0])
+
+#unordered,changeable and DO NOT ALLOW duplicate values
+
+dictt={
+       "name":"Raj",
+        "age":26,
+        "vehicle":"Ford endeavour",
+        "dob":"12-12-12"
+        }
+        
+print(dictt)
+print(len(dictt))
+print(type(dictt))
+
+x=dictt["vehicle"]
+print(x)
+x=dictt.get("vehicle")
+print(x)
+
+dictt["vehicle"]="BMW"
+
+print(dictt)
+
+dictt.update({"name":"Harsh"})
+print(dictt)
+
+dictt.pop("vehicle")
+
+fruits=["apples","pineapples"]
+fruits[0]
+print(fruits[0:1])
+#class and objects
+
+class Human:
+     x=5
+
+h1=Human()
+print(h1.x)
+
+def methods(sherlock):
+    print("hi,my name is"+"sherlock")
+
+x=lambda a:a+20
+print(x(5))
+
+def f1(w):
+    
+    return lambda a:a*n
+    
+    doub=f1(2)
+    
+    print(doub(11))
+    print(trip(11))    
+
+def square(x):
+    return x*x
+numbers=[1,2,3,4,5]
+print(x)
+
+import json
+
+person={"name":"Bob",
+        "languages":["English","French"]
+        }
+
+
+print(person)
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+     
+       
+          
+       
+      
+     
+        
+       
+       
+       
+                 
+       
+    
